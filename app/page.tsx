2e9ed9ddabc9a22f273eb621fb1e93570961ffa2
Lokeswaran A <11lokeswaran@gmail.com>
2025-08-22 23:58:27 +0530

    feat: Add prompt-kit's Input compoent

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { ChatInput } from "@/components/chat-input";
 import { Footer } from "@/components/footer";
 
 export default function Home() {
@@ -5,7 +6,7 @@ export default function Home() {
     <div className="max-w-4xl mx-auto relative size-full p-4 h-dvh">
       <main className="flex flex-col h-full items-center gap-2">
         <div className="h-full flex-1">Conversation</div>
-        <div className="mt-4">Prompt Input</div>
+        <ChatInput />
         <Footer />
       </main>
     </div>
