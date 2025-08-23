15d9e5fda68c6f17810ea86b60400c9ad8264652
Lokeswaran A <11lokeswaran@gmail.com>
2025-08-23 10:07:04 +0530

    refactor: Move Footer component to Chat

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import { Chat } from "@/components/chat";
-import { Footer } from "@/components/footer";
 
 export default function Home() {
   return (
     <div className="max-w-4xl mx-auto relative size-full p-4 h-dvh">
       <main className="flex flex-col h-full items-center gap-2">
         <Chat />
-        <Footer />
       </main>
     </div>
   );
--- components/chat.tsx
+++ components/chat.tsx
@@ -1,11 +1,13 @@
 import { ChatInput } from "./chat-input";
 import { Conversation } from "./conversation";
+import { Footer } from "./footer";
 
 export const Chat = () => {
   return (
     <>
       <Conversation />
       <ChatInput />
+      <Footer />
     </>
   );
 };
