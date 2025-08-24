e1f6ab2eb8c51b0db8578d41d99e9d8cf00ff570
Lokeswaran A <11lokeswaran@gmail.com>
2025-08-24 12:31:00 +0530

    feat: Extract header to a component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 import { ChatInput } from "@/components/chat-input";
 import { Footer } from "@/components/footer";
-import { GitHubLink } from "@/components/github-link";
+import { Header } from "@/components/header";
 import { MultiConversation } from "@/components/multi-conversation";
-import { ThemeSwitcher } from "@/components/theme-changer";
 
 export default function Home() {
   return (
     <div className="flex flex-col h-full max-h-full overflow-hidden">
-      <header className="flex h-16 shrink-0 items-center justify-end gap-2 border-b">
-        <div className="flex items-center gap-2 px-4">
-          <GitHubLink />
-          <ThemeSwitcher />
-        </div>
-      </header>
+      <Header />
       <main className="flex flex-col h-full max-h-full overflow-hidden">
         <div className="flex-1 overflow-hidden">
           <MultiConversation />
--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,12 @@
 import { GitHubLink } from "./github-link";
+import { ModelSelector } from "./model-selector";
 import { ThemeSwitcher } from "./theme-switcher";
 
 export const Header = () => {
   return (
     <header className="flex h-16 shrink-0 items-center justify-end gap-2 border-b">
       <div className="flex items-center gap-2 px-4">
+        <ModelSelector />
         <ThemeSwitcher />
         <GitHubLink />
       </div>
