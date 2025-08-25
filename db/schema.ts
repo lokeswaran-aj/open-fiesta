40047e2f7eeb48693d077728f7d734c64413f3d5
Lokeswaran A <11lokeswaran@gmail.com>
2025-08-25 14:57:24 +0530

    feat: Add google oauth in better auth configuration

--- db/schema.ts
+++ db/schema.ts
@@ -59,3 +59,10 @@ export const verification = pgTable("verification", {
     () => /* @__PURE__ */ new Date(),
   ),
 });
+
+export const schema = {
+  user,
+  session,
+  account,
+  verification,
+};
--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,2 +1,18 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient();
+
+export const signInWithGithub = async () => {
+  const data = await authClient.signIn.social({
+    provider: "github",
+  });
+  console.dir(data, { depth: null });
+  return data;
+};
+
+export const signInWithGoogle = async () => {
+  const data = await authClient.signIn.social({
+    provider: "google",
+  });
+  console.dir(data, { depth: null });
+  return data;
+};
