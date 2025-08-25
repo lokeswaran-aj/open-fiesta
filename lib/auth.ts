e0f843fcf3ffbbbf51fde31ca11b3e34b952ab67
Lokeswaran A <11lokeswaran@gmail.com>
2025-08-25 13:52:41 +0530

    feat: Enable github provider in Better auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,17 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
 import db from "@/db/drizzle";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [nextCookies()],
 });
