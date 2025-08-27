52d995e9a1b33111c24cbec3afce149797010695
Lokeswaran A <11lokeswaran@gmail.com>
2025-08-27 15:59:58 +0530

    feat: Update migrations

---
+++ migrations/0001_parallel_sharon_ventura.sql
@@ -0,0 +1,29 @@
+CREATE TABLE "chat" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"user_id" uuid NOT NULL,
+	"visibility" varchar DEFAULT 'private' NOT NULL,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "conversation" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"chat_id" uuid NOT NULL,
+	"model_id" varchar NOT NULL,
+	"active" boolean DEFAULT true NOT NULL,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "message" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"conversation_id" uuid NOT NULL,
+	"role" varchar NOT NULL,
+	"parts" json NOT NULL,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "chat" ADD CONSTRAINT "chat_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "conversation" ADD CONSTRAINT "conversation_chat_id_chat_id_fk" FOREIGN KEY ("chat_id") REFERENCES "public"."chat"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "message" ADD CONSTRAINT "message_conversation_id_conversation_id_fk" FOREIGN KEY ("conversation_id") REFERENCES "public"."conversation"("id") ON DELETE cascade ON UPDATE no action;
