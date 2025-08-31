219becf9668f1ce47ad07f3c7459d3b22f4f5e24
Lokeswaran A <11lokeswaran@gmail.com>
2025-08-31 10:30:27 +0530

    feat: Implement session validation in chat page

--- app/(chat)/c/[id]/page.tsx
+++ app/(chat)/c/[id]/page.tsx
@@ -1,5 +1,8 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { getChatWithConversationsWithMessages } from "@/actions/chat";
 import { Chat } from "@/components/chat";
+import { auth } from "@/lib/auth";
 
 type Props = {
   params: Promise<{
@@ -8,8 +11,17 @@ type Props = {
 };
 
 export default async function ChatPage(props: Props) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/auth");
+  }
+
   const { id } = await props.params;
   const chat = await getChatWithConversationsWithMessages(id);
+
   return (
     <main className="flex flex-col h-full max-h-full overflow-hidden">
       <Chat chat={chat} />
