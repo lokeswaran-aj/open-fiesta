0a8d7816984eab5d09f8750031468bef8817cfe7
Lokeswaran Aruljothy <74011196+lokeswaran-aj@users.noreply.github.com>
2025-09-01 14:14:26 +0530

    feat: Add posthog analytics (#13)

---
+++ instrumentation-client.ts
@@ -0,0 +1,12 @@
+import posthog from "posthog-js";
+
+if (
+  process.env.NODE_ENV === "production" &&
+  process.env.NEXT_PUBLIC_POSTHOG_KEY &&
+  process.env.NEXT_PUBLIC_POSTHOG_HOST
+) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    defaults: "2025-05-24",
+  });
+}
